0455c7640efd3d993e8523ee69935dc71323df0d
dongqiangqiang <1521115016@qq.com>
2015-07-09 17:32:29 +0800

    update the sendIos

--- src/main/java/com/nana/webapi/servlet/AppPushServlet.java
+++ src/main/java/com/nana/webapi/servlet/AppPushServlet.java
@@ -93,9 +93,9 @@ public class AppPushServlet extends HttpServlet {
 
 		List<String> tokens = new ArrayList<String>();
 		tokens.add(rm.getId());
-		System.out.println(rm.getId() + "  " + rm.getAudioText());
 		// String path = "IntelligencePush.p12";
-		String message = rm.getAudioText();//JSON.toJSONString(rm);
+		String message = JSON.toJSONString(rm);
+		System.out.println(rm.getId() + "  " + message);
 		sendIOS(tokens, message);
 	}
 
